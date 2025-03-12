0f6d04c2e3c8a97889cdbfb24924d71bb3b7860a
EOhenhen <ohenheneno@gmail.com>
2025-03-12 11:22:31 -0400

    Adding baseline search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,11 @@
 const Search = () => {
     return (
-<>
-
-</>
+        <>
+            <label for="searchString">Who you looking for? <span class="small">
+                (Regular expressions are cool here)
+            </span></label>
+            <input id="searchString" oninput="filterCharacters()" autocomplete="off" />
+        </>
     )
 };
 
